b57b19d63f5b3c512fffbb94e50013f40f63be6b
Steinchen001 <azromanova@gmx.net>
2026-06-28 21:25:30 +0100

    app.js aktualisieren

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const version = document.getElementById("app-version");
-
-  if(version && typeof Version !== "undefined"){
-    version.innerText = "ARKANUS ENGINE // " + Version.getLabel();
-  }
-
   if(typeof Arkanus !== "undefined"){
     Arkanus.init();
   }else{
